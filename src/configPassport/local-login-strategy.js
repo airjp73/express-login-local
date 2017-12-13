a3bc555c1b4ace64855fdf1cb62d9afc46830693
AaronP <aaron.pettengill.violin@gmail.com>
2017-12-12 20:35:37 -0500

    local-login tests

--- src/configPassport/local-login-strategy.js
+++ src/configPassport/local-login-strategy.js
@@ -11,7 +11,8 @@ module.exports = (config) => {
   },
   async (req, email, password, done) => {
     try {
-
+      console.log(email)
+      console.log(password)
       var user = await config.database.getUser({[con.fields.EMAIL]: email}, [con.fields.PASSWORD])
 
       if (!user)
--- src/configPassport/local-login-strategy.spec.js
+++ src/configPassport/local-login-strategy.spec.js
@@ -2,53 +2,84 @@
 
 var sinon = require('sinon')
 var chai = require('chai')
-chai.use(require('chai-passport-strategy'))
+var expect = chai.expect
 
+//mocks
 var sandbox = sinon.createSandbox()
-var mockReq = {
+var mockUser = {
   email: "test@test.com",
   password: "testpass"
 }
 var mockConfig = {
   database: {
     newUser: sandbox.stub(),
-    getUser: sandbox.stub(),
-    saveUser: sandbox.stub()
+    saveUser: sandbox.stub(),
+    getUser: sandbox.stub().callsFake((selection, projection = []) => {
+      if (selection.email != mockUser.email)
+        return undefined
+      return mockUser
+    })
+  },
+  encrypt: {
+    matchPassword: sandbox.stub().callsFake((pass1, pass2) => {
+      return pass1 == pass2
+    })
   }
 }
-var strategy = require('./local-login-strategy')(mockConfig)
 
-var user, info
+var strategy = require('./local-login-strategy')(mockConfig)
+strategy.success = sandbox.stub()
+strategy.fail = sandbox.stub()
+strategy.error = sandbox.stub()
+
 
 describe("local-login-strategy", () => {
 
-  before(() => {
-    chai.passport.use(strategy)
-      .success((u, i) => {
-        user = u
-        info = i
-        done()
-      })
-      .fail((i) => {
-        console.log(i)
-      })
-      .error((err) => {
-        console.log(err)
-      })
-      .req((req) => {
-        req.body = {}
-        req.body.email = mockReq.email
-        req.body.password =mockReq.password
-      })
-      .authenticate()
-  })
-
   beforeEach(() => {
-    sandbox.reset()
+    sandbox.resetHistory()
   })
 
-  it("should return user with no password", () => {
-    console.log(user)
-    console.log(info)
+  it("should return user with no password", async () => {
+    var req = {
+      body: {
+        email: mockUser.email,
+        password: mockUser.password
+      }
+    }
+    await strategy.authenticate(req)
+
+    sinon.assert.notCalled(strategy.fail)
+    sinon.assert.calledOnce(strategy.success)
+
+    var user = strategy.success.getCall(0).args[0]
+    expect(user.email).to.equal(mockUser.email)
+    expect(user.password).to.be.undefined
   })
+
+  it("should 401 if bad password", async () => {
+    var req = {
+      body: {
+        email: mockUser.email,
+        password: "nomatch"
+      }
+    }
+    await strategy.authenticate(req)
+
+    sinon.assert.notCalled(strategy.success)
+    sinon.assert.calledOnce(strategy.fail)
+  })
+
+  it("should fail if no matching user", async () => {
+    var req = {
+      body: {
+        email: "nomatch@email.com",
+        password: mockUser.password
+      }
+    }
+    await strategy.authenticate(req)
+
+    sinon.assert.notCalled(strategy.success)
+    sinon.assert.calledOnce(strategy.fail)
+  })
+
 })
