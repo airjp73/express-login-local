5f12f68aa610ae5c01e780925681a8dd52e201cf
AaronP <aaron.pettengill.violin@gmail.com>
2017-12-22 17:42:34 -0500

    Better tests for configPassport index.js

--- src/configPassport/index.js
+++ src/configPassport/index.js
@@ -1,9 +1,11 @@
 "use strict"
 
+var con = require('../constants')
+
 module.exports = (config, passport) => {
   var localSignupStrategy = require('./local-signup-strategy')(config)
   var localLoginStrategy  = require('./local-login-strategy')(config)
-  
+
   passport.use(con.passport.LOCAL_SIGNUP,  localSignupStrategy)
   passport.use(con.passport.LOCAL_LOGIN, localLoginStrategy)
 }
--- src/configPassport/index.spec.js
+++ src/configPassport/index.spec.js
@@ -1,10 +1,40 @@
 var chai = require('chai')
 var expect = chai.expect
+var proxyquire = require('proxyquire')
+var sinon = require('sinon')
+
+var con = require('../constants')
+
+
+var loginMock = {hi: "hi"}
+var signupMock = {bye: "bye"}
+proxyquire('./index.js', {
+  './local-login-strategy' : (config) => {
+    return loginMock
+  },
+  './local-signup-strategy' : (config) => {
+    return signupMock
+  }
+})
+var passport = {
+  use: sinon.spy()
+}
 
 var ex = require('./index.js')
 
 describe("configPassport", () => {
   it("should export a function", () => {
     expect(ex).to.be.a('function')
+  })
+
+  describe("function behavior", () => {
+    before(() => {
+      var config = {}
+      ex(config, passport)
+    })
+    it("should call passport.use", () => {
+      sinon.assert.calledWith(passport.use, con.passport.LOCAL_LOGIN, loginMock)
+      sinon.assert.calledWith(passport.use, con.passport.LOCAL_SIGNUP, signupMock)
+    })
   })
 })
