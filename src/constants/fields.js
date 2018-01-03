f4f07faa17ace2da09ff400ea52433e94687818f
airjp73 <aaron.pettengill.violin@gmail.com>
2018-01-03 10:21:09 -0500

    Removed OAuth2 token field
    Oath2 will be in it's own strategy module.

--- src/constants/fields.js
+++ src/constants/fields.js
@@ -5,7 +5,5 @@ module.exports = {
   CONFIRM_EMAIL_TOKEN    : "confirmEmailToken",
   EMAIL_CONFIRMED        : "emailConfirmed",
   RESET_PASSWORD_TOKEN   : "resetPasswordToken",
-  RESET_PASSWORD_EXPIRES : "resetPasswordExpires",
-
-  OAUTH2_TOKEN : "oauth2Token"
+  RESET_PASSWORD_EXPIRES : "resetPasswordExpires"
 }
